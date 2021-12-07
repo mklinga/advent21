0cbd7e74e34dda2f6008e0871168996799b00410
Markus Klinga <mail@mklinga.com>
2021-12-07 10:36:54 +0100

    Advent 7, whales

---
+++ 7/7.2.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+function getInput() { 
+    return new Promise((resolve, reject) => {
+        fs.readFile('./input1.txt', {}, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve(data.toString().split('\n'));
+        });
+    });
+}
+
+async function process() {
+    const input = await getInput();
+    if (!Array.isArray(input)) {
+        throw new Error('Input is not an array!')
+    }
+
+    const positions = input[0].trim().split(',').map(x => parseInt(x)).reduce((acc, p) => ({ ...acc, [p]: (acc[p] || 0) + 1 }), {});
+
+    const largest = Math.max.apply(null, Object.keys(positions));
+    let least = { x: undefined, sum: Number.POSITIVE_INFINITY };
+    function consumption(currentPosition, target) {
+        const distanceToMove = Math.abs(currentPosition - target);
+        let costOfMove = 1;
+        let totalCost = 0;
+        for (let i = 0; i < distanceToMove; i++) {
+            totalCost += costOfMove;
+            costOfMove++;
+        }
+        return totalCost;
+    }
+
+    for (let x = 0; x < largest; x++) {
+        const fuel = Object.entries(positions).reduce((sum, [k, v]) => {
+            return sum + (v * consumption(k, x));
+        }, 0);
+        if (fuel < least.sum) {
+            least = { sum: fuel, number: x };
+        }
+    }
+
+    console.log('least', least)
+}
+
+process();
